26816e40ca745c6fc5f7799cf144f2a6bf682d1e
BandFlow25 <jason@flowency.co.uk>
2026-01-06 12:06:09 +0000

    chore: remove debug code and re-enable Amplify cache

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,20 +3,6 @@ import App from "./App";
 import "./index.css";
 import { setupChunkReloadHandler } from "./lib/chunk-reload-handler";
 
-// DEBUG: Catch the enabled error via global error handler
-window.addEventListener('error', (event) => {
-  if (event.message?.includes('enabled to be a boolean')) {
-    console.log('[DEBUG] ERROR CAUGHT! Full details:', {
-      message: event.message,
-      filename: event.filename,
-      lineno: event.lineno,
-      colno: event.colno,
-      error: event.error,
-      stack: event.error?.stack,
-    });
-  }
-});
-
 // Setup auto-reload for chunk load failures (e.g., after new deployments)
 setupChunkReloadHandler();
 
