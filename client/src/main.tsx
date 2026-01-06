f8df6d5771e04b4601ab6bcaa9f62dbea9907720
BandFlow25 <jason@flowency.co.uk>
2026-01-06 11:42:14 +0000

    Add debug trace for enabled error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,16 @@ import App from "./App";
 import "./index.css";
 import { setupChunkReloadHandler } from "./lib/chunk-reload-handler";
 
+// DEBUG: Catch the enabled error and log full stack trace
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0]?.toString?.().includes('enabled to be a boolean')) {
+    console.log('[DEBUG] CAUGHT THE ERROR! Stack trace:');
+    console.trace();
+  }
+  originalError.apply(console, args);
+};
+
 // Setup auto-reload for chunk load failures (e.g., after new deployments)
 setupChunkReloadHandler();
 
