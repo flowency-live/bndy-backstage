ad1a77471e1861c1c3cfdc6c0182e3bf75318b84
BandFlow25 <jason@flowency.co.uk>
2026-08-31 21:29:12 +0100

    fix: update vite config to use SVG favicon

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
         name: 'BNDY Backstage',
         short_name: 'bndy',
